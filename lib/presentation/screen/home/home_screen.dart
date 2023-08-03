6289eeafd7d5023897ee765cce63a7b830ccb08e
fathrahh <gazoon3@gmail.com>
2023-08-04 00:01:03 +0700

    continue watching

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -6,6 +6,8 @@ import 'package:filmio/presentation/screen/home/widgets/home_movie_list.dart';
 import 'package:filmio/presentation/screen/home/widgets/home_carousel.dart';
 import 'package:filmio/common_widgets/app_bottom_navigation.dart';
 
+import 'dart:math';
+
 class MyHomePage extends StatefulWidget {
   final String title;
   const MyHomePage({super.key, required this.title});
@@ -61,74 +63,22 @@ class _MyHomePageState extends State<MyHomePage> {
                       const SizedBox(
                         height: 16,
                       ),
-                      Row(
-                        children: List.generate(10, (index) {
-                          return Container(
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(8.0),
-                              ),
-                            ),
-                            clipBehavior: Clip.antiAlias,
-                            width: 180,
-                            child: Column(
-                              children: [
-                                SizedBox(
-                                  height: 80,
-                                  child: Stack(
-                                    fit: StackFit.expand,
-                                    children: [
-                                      Image.asset(
-                                        'images/hp-preview.jpg',
-                                        fit: BoxFit.cover,
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Expanded(
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            const Text(
-                                              "Hospital Playlist",
-                                              style: TextStyle(
-                                                fontSize: 12,
-                                                fontWeight: FontWeight.w500,
-                                              ),
-                                            ),
-                                            Text(
-                                              "Eps 4, Season 2",
-                                              style: TextStyle(
-                                                color: ThemeColors
-                                                    .primary.shade100,
-                                                fontSize: 10,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      SvgPicture.asset(
-                                        'assets/icons/more.svg',
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          );
-                        }),
+                      SizedBox(
+                        height: 130,
+                        child: ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 10,
+                          separatorBuilder: (context, index) =>
+                              const SizedBox(width: 8),
+                          itemBuilder: (context, index) {
+                            return const ContinueWatchWidget();
+                          },
+                        ),
                       )
                     ],
                   ),
                   const SizedBox(
-                    height: 16,
+                    height: 12,
                   ),
                   const HomeMovieList(),
                 ],
@@ -136,6 +86,104 @@ class _MyHomePageState extends State<MyHomePage> {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ContinueWatchWidget extends StatelessWidget {
+  const ContinueWatchWidget({
+    super.key,
+  });
+
+  final double widthCard = 180;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(8.0),
+        ),
+      ),
+      clipBehavior: Clip.antiAlias,
+      width: widthCard,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 80,
+            child: Stack(
+              alignment: Alignment.center,
+              fit: StackFit.expand,
+              children: [
+                Image.asset(
+                  'images/hp-preview.jpg',
+                  fit: BoxFit.cover,
+                ),
+                Align(
+                  child: Container(
+                    width: 36,
+                    height: 36,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.2),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(16.0),
+                      ),
+                    ),
+                    child: SvgPicture.asset(
+                      'assets/icons/play-bold.svg',
+                      height: 22,
+                      width: 22,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Container(
+                    color: ThemeColors.white,
+                    height: 2,
+                    // This value just for template change later
+                    // if got value from API
+                    width: widthCard * Random().nextDouble(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Hospital Playlist",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        "Eps 4, Season 2",
+                        style: TextStyle(
+                          color: ThemeColors.primary.shade100,
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SvgPicture.asset(
+                  'assets/icons/more.svg',
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
